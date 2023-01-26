b27fa325ff84216f960bca46710364ccdde49752
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2023-01-26 15:08:52 +0100

    route

--- routes/Exam.js
+++ routes/Exam.js
@@ -18,7 +18,7 @@ router.route("/:id/passcode-verify").post(protect, verifyExamSchedulePassCode);
 router.route("/sections/:id").get(protect, getSections);
 router.route("/questions/:id").get(protect, getQuestions);
 router.route("/:id/instruction").get(protect, getExamInstruction);
-router.route("/start-exam:id").put(protect, startExam);
+router.route("/start/:id").put(protect, startExam);
 
 router
   .route(
