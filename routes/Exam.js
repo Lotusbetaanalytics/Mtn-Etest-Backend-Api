aa83cf8e684d903fdfda6223ac0f25f87a118f6a
judeakinwale <judeakinwale@gmail.com>
2023-02-10 06:47:28 +0100

    Minor Changes

--- routes/Exam.js
+++ routes/Exam.js
@@ -3,6 +3,7 @@ const {
   getMyExam,
   getSections,
   getQuestions,
+  getExamQuestions,
   answerQuestion,
   verifyExamPassCode: verifyExamSchedulePassCode,
   getExamInstruction,
@@ -18,8 +19,8 @@ router.route("/single/:id").get(protect, getSingleExam);
 router.route("/:id/passcode-verify").post(protect, verifyExamSchedulePassCode);
 router.route("/sections/:id").get(protect, getSections);
 router.route("/section-detail/:id").get(protect, getSectionDetails);
-// router.route("/questions/:id").get(protect, getQuestions);
-router.route("/questions/:id/:examSchedId").get(protect, getQuestions);
+router.route("/questions/:id").get(protect, getQuestions);
+router.route("/questions/:id/:examSchedId").get(protect, getExamQuestions);
 router.route("/:id/instruction").get(protect, getExamInstruction);
 router.route("/start/:id").put(protect, startExam);
 
